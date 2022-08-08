c654e8e04f6a80c8e3b9fd630fd5ce37593d25cd
Efe Polat <mefepolat@hotmail.com>
2022-08-07 22:28:51 -0400

    implemented joi and validation schemas

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
+const {campgroundSchema} = require('./schemas.js')
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', 
 {   useNewUrlParser: true, 
@@ -23,8 +25,19 @@ db.once('open', () => {
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
+
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req,res,next) => {
+    const {error} = campgroundSchema.validate(req.body)
+    if(error){
+        const message = error.details.map(el => el.message).join(',')
+        throw new ExpressError(message, 400)
+    } else {
+        next();
+    }
+}
 
 app.get('/', (req,res) => {
 
@@ -41,8 +54,9 @@ app.get('/campgrounds/new', (req,res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req,res,next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req,res,next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -54,7 +68,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req,res,next) => {
     res.render('campgrounds/edit', {campground})
 }))
 
-app.put('/campgrounds/:id', catchAsync(async(req,res,next) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req,res,next) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
